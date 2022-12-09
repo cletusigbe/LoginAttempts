f942d2804401f683748c0815e7bdb14d17135532
cletus igbe <cletus.igbe@gmail.com>
2022-12-09 06:13:41 +0100

    How to Create 3 Attempts Login System in C#

---
+++ LoginAttempts/Login.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace LoginAttempts
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans Semibold", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans Semibold", 9.25F);
+            this.label2.Location = new System.Drawing.Point(59, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Open Sans Semibold", 9.25F);
+            this.txtUsername.Location = new System.Drawing.Point(138, 53);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(241, 24);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.Font = new System.Drawing.Font("Open Sans Semibold", 9.25F);
+            this.txtpwd.Location = new System.Drawing.Point(138, 94);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.PasswordChar = '*';
+            this.txtpwd.Size = new System.Drawing.Size(241, 24);
+            this.txtpwd.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Open Sans Semibold", 9.25F);
+            this.btnLogin.Location = new System.Drawing.Point(304, 137);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 28);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "&Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(426, 184);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 225);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Attempts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ LoginAttempts/Login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginAttempts
+{
+    public partial class Login : Form
+    {
+        public int logAttempt = 0;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "demo" && txtpwd.Text == "demo123")
+            {
+                //Do something
+                this.Hide();
+                Main main = new Main();
+                main.Show();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Wrong Username or Password");
+                txtpwd.Text = "";
+                txtUsername.Text = "";
+                txtUsername.Focus();
+
+                logAttempt += 1;
+
+                if(logAttempt == 3)
+                {
+                    MessageBox.Show("You have exceeded the maximum number of attempts", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    btnLogin.Enabled = false;
+                    txtpwd.Text = "";
+                    txtUsername.Text = "";
+                    txtpwd.Enabled = false;
+                    txtUsername.Enabled = false;
+                }
+
+            }
+        }
+    }
+}
